baf1f4e30e324de1ba764216199030adf1190c54
Maks-vell <maks_vell25@mail.ru>
2021-02-17 13:03:08 +0300

    Init commit

---
+++ Source/TheExpansion/TheExpansionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheExpansionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEEXPANSION_API ATheExpansionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
